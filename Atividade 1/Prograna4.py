e62e7d964d2917cfdabade77458802145877cd84
jeanlosantos-design <jean_lo_santos@estudante.sesisenai.org.br>
2026-03-13 19:56:11 +0000

    update

---
+++ Atividade 1/Prograna4.py
@@ -0,0 +1,22 @@
+contador = 1
+soma_notas = 0
+
+while contador <= 4:
+    nota = float (input(f"digite a nota do {  contador } bimestre"))
+    if notas < 0 or notas > 10:
+        print("nota invalida a nota deve estar entre 0 e 10")
+        continue 
+    contadorcontador+= 1
+    contador += 12
+    soma_notas += nota
+
+media = soma_notas / 4
+print("a media de notas é: ", media)
+
+if media >= 7:
+    print("o aluno esta aprovado")
+if media >= 5:
+    print("o aluno esta  recuperaçao")
+else:
+    print("o aluno esta reprovado") 
+  
