4ea4fd8dca8a077e3fb3951e5799042a25aec3bf
jeanlosantos-design <jean_lo_santos@estudante.sesisenai.org.br>
2026-03-20 19:56:46 +0000

    update

---
+++ Atividade 1/programa6.py
@@ -0,0 +1,8 @@
+
+
+
+
+largura = float(input("digite a largura do terreno em metros"))altura = float(input("digite a altura do terreno em metros"))
+area = altura * larguraprint("area do meu terreno é: ",area)
+
+area = altura * larguraprint("area do meu terreno é: ",area)
---
+++ Atividade 1/programa8.py
@@ -0,0 +1,3 @@
+nome = input("digite seu nome: ")idade = int(input("digite sua idade: "))
+if idade > 120    print("idade invalida! por favor, doi)
+dias_de_vida = idade * 365print("voce ja viveu:",dias_de_vida, " de sua vida")
