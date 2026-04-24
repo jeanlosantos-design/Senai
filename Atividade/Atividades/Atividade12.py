d0c1ed4e8210f77ff41a8172fe941df7453d9c6d
jeanlosantos-design <jean_lo_santos@estudante.sesisenai.org.br>
2026-04-24 20:15:55 +0000

    update

---
+++ Atividade/Atividades/Atividade12.py
@@ -0,0 +1,7 @@
+salario = float("digitw seu salario atual:  ")
+x = salario * 0.15
+aumento = aumento * 0.08
+salariofinal = aumento - imposto
+print(" o salario sem aumento era:",salario)
+print(" o salario com aumento ficou:",salario)
+print(" o salario final ficou :",salarioi)
---
+++ Atividade/Atividades/Atividade13.py
@@ -0,0 +1,5 @@
+numero = input("Insira numeros inteiros de no maximo 3 digitos")
+
+print(f"Centena: {numero[:2]}")
+print(f"Dezena: {numero[1:2]}")
+print(f"Unidade: {numero[2:]}")
